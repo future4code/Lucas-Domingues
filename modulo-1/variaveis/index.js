3bedda30b13f2a985957f2419169005e2b92f0e5
unknown <elesouzadom@gmail.com>
2021-10-04 16:36:57 -0400

    Exercicio incompleto (aula variaveis)

---
+++ modulo-1/variaveis/index.js
@@ -0,0 +1,19 @@
+/*
+ 1) será impresso os resultados atribuidos As variaveis a e b , em  a será =10 , e em b como foi lhe atribuido outro valor diferente do começo que resultara em 5
+2)Será imprimido 10,10,10 , pois as atribuições feitas em todas as variaveis resultará no numero dez.
+
+let horasDeTrabalhoPorDia= prompt("Quantas horas você trabalha por dia?")
+let reciboDiario = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${reciboDiario/horasDeTrabalhoPorDia} por hora`).*/
+
+
+//let qualSeuNome ;
+//let qualSuaIdade ;
+//console.log(typeof minhaVariavel)
+/* erro pois a variavel não foi definido nenhum valor a ela .
+
+let Nome = prompt ("Digite seu nome ?")
+let idade= prompt ("Qual sua idade ?")
+string , e int , deu esses resultados pois são os tipos atribuidos há variavel .
+console.log ("Olá", nome , "você tem", idade ,"anos ")
+
