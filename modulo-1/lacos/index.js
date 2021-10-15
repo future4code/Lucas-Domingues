d590d6854faaa73d156ea04083ce009f6153ac5c
DesenvolvedorDOM23 <elesouzadom@gmail.com>
2021-10-14 23:26:18 -0300

    não consegui realizar os exercicios de escrita de codigo de lacos

---
+++ modulo-1/lacos/index.js
@@ -0,0 +1,14 @@
+/*
+o codigo está reptindo uma ação que foi dada no bloco de codigo , até que se chegue a um resultado esperado ,
+o resultado que aparecera no console será igual a 10. 
+2)será impresso no console todos os numeros maiores que estão dentro do array.
+sim , só trocar o >18 por ! de zero .
+3) seria impresso o numero 6*/
+
+let pets = Number(prompt("digite a quantidade de pets que você tem ??"))
+if (pets === 0){
+    console.log("que pena você pode adotar um pet!!")
+}else {
+  let nomePets=Number(prompt("digite nome dos seus pets "))
+}
+console.log( nomePets)
