9307fbb58ac8af316c425e304bcc3a059fb30bf5
lucas de souza domingues <elesouzadom@gmail.com>
2022-01-27 20:09:40 -0300

    resolução dos exercicios

---
+++ modulo-4/intro-express/src/exercicio2.ts
@@ -0,0 +1,30 @@
+export type user ={
+    id: number,
+    name: string,
+    phone: string,
+    email: string,
+    website: string
+}
+export const users : user[]=[
+    {
+        id: 1,
+        name: "Leanne Graham",
+        email: "Sincere@april.biz",
+        phone: "1-770-736-8031 x56442",
+        website: "hildegard.org",
+      },
+      {
+        id: 2,
+        name: "Ervin Howell",
+        email: "Shanna@melissa.tv",
+        phone: "010-692-6593 x09125",
+        website: "anastasia.net",
+      },
+      {
+        id: 3,
+        name: "Clementine Bauch",
+        email: "Nathan@yesenia.net",
+        phone: "1-463-123-4447",
+        website: "ramiro.info",
+      },
+  ]
--- modulo-4/intro-express/src/index.ts
+++ modulo-4/intro-express/src/index.ts
@@ -1,59 +1,35 @@
-import express from 'express'
-import cors from 'cors'
-import users from "../data"
-const app = express() 
-
+import express from "express"
+import cors from "cors"
+import { users } from "./exercicio2"
+import { Posts } from "./exercicios567"
+const app = express ()
 
 app.use(cors())
-app.use(express.json())
+app.use (express.json())
 
+app.listen(3003, ()=>console.log("servidor rodando"))
 
-
-   app.get("/", (req , res) => {  
-    console.log("bateu")        
-    res.status(200) .send("ok")
+app.get ("/",(req,res) =>{
+    res.send ("OK")
 })
 
+app.get ("/users",(req, res ) =>{
+    res.send (users)
+})
 
-app.listen(3003, () => {
-    console.log("servidor rodando na porta 3003");
-   });
- 
- type post ={
-     UserId: number,
-     id: number,
-     title: string,
-     body : string
- }
-   type Users = {
-       id: number,
-       name: string,
-       phone: number,
-       email: string,
-       website: string,
+app.get("/Posts",(req,res) =>{
+    res.send (Posts)
+})
+app.get("/users/:id/posts", (req, res) => {
+  const userId = Number(req.params.id)
 
-   }
-const usuarios : Users ={
-    id : 1,
-   name : "silva",
-    phone: 699999-888,
-    email : "ele@gmail.com",
-    website : "http//: securyti.org.com"
-}
- const posts : post= {
-    
-       UserId : 3,
-        id: 25,
-        title: "rem alias distinctio quo quis",
-        body:"ullam consequatur ut\nomnis quis sit vel consequuntur\nipsa eligendi ipsum molestiae et omnis error nostrum\nmolestiae illo tempore quia et distinctio"
-          
- }
-
-app.get("/userId",(req,res) =>{
-  const currentUsers = users 
-  const userId = currentUsers.map((users:any)=>{
-          return users.userId
+  const result = Posts.filter(Post => {
+    if (Post.userId === userId) {
+      return true
+    } else {
+      return false
+    }
   })
-  const result = users .flat (1)
-   res.status(200).send(result)
+
+  res.send(result)
 })
