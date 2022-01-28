4fbc6f759999ad14c8913d9e8a37ee540a6528ad
lucas de souza domingues <elesouzadom@gmail.com>
2022-01-27 21:11:55 -0300

    exercicios resolvidos

---
+++ modulo-4/aprofundamento-express/src/exercico45.ts
@@ -0,0 +1,27 @@
+export type Post = {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+export const posts: Post[] = [
+    {
+      userId: 1,
+      id: 11,
+      title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      userId: 2,
+      id: 22,
+      title: "qui est esse",
+      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+      userId: 3,
+      id: 33,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
+]
